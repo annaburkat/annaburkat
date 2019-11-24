3225414f6e0604d28fbccb632a2542638cc96f34
Anna <ankaburkat@gmail.com>
2019-11-24 23:23:24 +0000

    homepage-first version

--- src/js/script.js
+++ src/js/script.js
@@ -2,9 +2,17 @@ const myFunction = () => {
   $('.navbar').toggleClass("responsive");
 }
 
-$(function () {
-  $(document).scroll(function () {
+$(function() {
+  $(document).scroll(function() {
     var navbar = $(".js-navbar");
     navbar.toggleClass('navbar--scrolled', $(this).scrollTop() > navbar.height());
   });
 });
+
+let x = -1;
+setInterval(function() {
+  let images = ['breakfast.jpg', 'breakfast-dark.jpg', 'soup.jpg', 'shake.jpg', 'watermelon.jpg'];
+  x = (x === images.length - 1) ? 0 : x + 1;
+  let new_image = '/images/' + images[x];
+  $('.js-intro__image').attr('src', new_image);
+}, 4000);
