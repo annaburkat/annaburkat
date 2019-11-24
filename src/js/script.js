5e18574db2fd559a7b4b8a6f92dd425d5b7ca109
Anna <ankaburkat@gmail.com>
2019-11-24 01:24:57 +0000

    homepage start

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,10 @@
 const myFunction = () => {
- $('.navbar').toggleClass("responsive");
+  $('.navbar').toggleClass("responsive");
 }
+
+$(function () {
+  $(document).scroll(function () {
+    var navbar = $(".js-navbar");
+    navbar.toggleClass('navbar--scrolled', $(this).scrollTop() > navbar.height());
+  });
+});
