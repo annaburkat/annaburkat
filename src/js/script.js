8ee8f40b0e096a41f10b47311e2780eb65f99cf4
Anna <ankaburkat@gmail.com>
2019-12-07 16:42:51 +0000

    resize images, refactor code

--- src/js/script.js
+++ src/js/script.js
@@ -33,16 +33,16 @@ function plusSlides(n) {
 
   function showSlides(n) {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
+    var slides = document.getElementsByClassName("js-mythbusted-slide");
+    var dots = document.getElementsByClassName("js-mythbusted__dot");
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+        dots[i].className = dots[i].className.replace(" mythbusted-slider__dot--active", "");
     }
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
+    dots[slideIndex-1].className += " mythbusted-slider__dot--active";
   }
