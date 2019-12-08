f510a8e327886c28f3a18fa4f748b610076872a2
Anna <ankaburkat@gmail.com>
2019-12-08 12:24:24 +0000

    fixes Alex

--- src/js/script.js
+++ src/js/script.js
@@ -1,42 +1,42 @@
-const myFunction = () => {
-  $('.navbar').toggleClass("responsive");
-}
+$(document).ready(function() {
+  const myFunction = () => {
+    $('.navbar').toggleClass("responsive");
+  }
 
-$(function() {
-  $(document).scroll(function() {
-    var navbar = $(".js-navbar");
-    navbar.toggleClass('navbar--scrolled', $(this).scrollTop() > navbar.height());
+  $(function() {
+    $(document).scroll(function() {
+      var navbar = $(".js-navbar");
+      navbar.toggleClass('navbar--scrolled', $(this).scrollTop() > navbar.height());
+    });
   });
-});
 
-let x = -1;
-setInterval(function() {
-  let images = ['breakfast.jpg', 'breakfast-dark.jpg', 'soup.jpg', 'shake.jpg', 'watermelon.jpg'];
-  x = (x === images.length - 1) ? 0 : x + 1;
-  let new_image = '/images/' + images[x];
-  $('.js-intro__image').attr('src', new_image);
-}, 4000);
+  let x = -1;
+  setInterval(function() {
+    let images = ['breakfast.jpg', 'breakfast-dark.jpg', 'soup.jpg', 'shake.jpg', 'watermelon.jpg'];
+    x = (x === images.length - 1) ? 0 : x + 1;
+    let new_image = '/images/' + images[x];
+    $('.js-intro__image').attr('src', new_image);
+  }, 4000);
 
-// ==========| Alex Scrip |========== //
+  // ==========| Alex Scrip |========== //
 
-// ---------- | links animation | ----------  //
+  // ---------- | links animation | ----------  //
 
-$(document).ready(function(){
- 
-	$('.go_up').click(function(){
-		$('body, html').animate({
-			scrollTop: '0px'
-		}, 300);
-	});
- 
-	$(window).scroll(function(){
-		if( $(this).scrollTop() > 0 ){
-			$('.go_up').slideDown(300);
-		} else {
-			$('.go_up').slideUp(300);
-		}
-	});
- 
+
+  $('.go_up').click(function() {
+    $('body, html').animate({
+      scrollTop: '0px'
+    }, 300);
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 0) {
+      $('.go_up').slideDown(300);
+    } else {
+      $('.go_up').slideUp(300);
+    }
+  });
+
 
   $('a[href^="#"]').click(function() {
     var destino = $(this.hash);
@@ -46,9 +46,12 @@ $(document).ready(function(){
     if (destino.length == 0) {
       destino = $('html');
     }
-    $('html, body').animate({ scrollTop: destino.offset().top }, 500);
+    $('html, body').animate({
+      scrollTop: destino.offset().top -50
+    }, 500);
     return false;
   });
+
 });
 
 
@@ -58,29 +61,29 @@ $(document).ready(function(){
 
 
 
- /* (function () {
-            var previousScroll = 0;
+/* (function () {
+           var previousScroll = 0;
 
-            $(window).scroll(function(){
-                var currentScroll = $(this).scrollTop();
-                
-                if (currentScroll > previousScroll){
-                    $('.grow_bar').css('width','+=10px')
-                } else {
-                    $('.grow_bar').css('width','-=10px')
-                }
-                previousScroll = currentScroll;
-                
-            });
-            
-            var lastScrollTop = 0;
-                $(window).scroll(function(event){
-                   var st = $(this).scrollTop();
-                   if (st > lastScrollTop){
-                       // downscroll code
-                   } else {
-                      // upscroll code
-                   }
-                   lastScrollTop = st;
-                });
-         }()); */
+           $(window).scroll(function(){
+               var currentScroll = $(this).scrollTop();
+
+               if (currentScroll > previousScroll){
+                   $('.grow_bar').css('width','+=10px')
+               } else {
+                   $('.grow_bar').css('width','-=10px')
+               }
+               previousScroll = currentScroll;
+
+           });
+
+           var lastScrollTop = 0;
+               $(window).scroll(function(event){
+                  var st = $(this).scrollTop();
+                  if (st > lastScrollTop){
+                      // downscroll code
+                  } else {
+                     // upscroll code
+                  }
+                  lastScrollTop = st;
+               });
+        }()); */
