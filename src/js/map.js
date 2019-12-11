a16711f8cd425c1b18ff0499782dd76aabe1b5a7
Conor <conormoloneycm@gmail.com>
2019-12-11 14:22:05 +0000

    added more info to the calculator page

--- src/js/map.js
+++ src/js/map.js
@@ -15,22 +15,22 @@ function initMap() {
 }
 
 
-//Conor - calculator
+// calculator
 function computeBMI() {
-  // user inputs
+  // inputs
   var height = Number(document.getElementById("height").value);
   var heightunits = document.getElementById("heightunits").value;
   var weight = Number(document.getElementById("weight").value);
   var weightunits = document.getElementById("weightunits").value;
 
 
-  //Convert all units to metric
-  if (heightunits == "inches") height /= 0.393700787;
-  if (weightunits == "lb") weight /= 2.20462;
+  //Convert to metric
+  if (heightunits === "inches") height /= 0.393700787;
+  if (weightunits === "lb") weight /= 2.20462;
 
   //Perform calculation
 
-  //        var BMI = weight /Math.pow(height, 2)*10000;
+  // var BMI = weight /Math.pow(height, 2)*10000;
   var BMI = Math.round(weight / Math.pow(height, 2) * 10000);
 
   //Display result of calculation
@@ -38,12 +38,12 @@ function computeBMI() {
 
   var output = Math.round(BMI * 100) / 100
   if (output < 18.5)
-    document.getElementById("comment").innerText = "Underweight";
+    document.getElementById("remark").innerText = "You are Underweight, we recommend a higher calorie count";
   else if (output >= 18.5 && output <= 25)
-    document.getElementById("comment").innerText = "Normal";
+    document.getElementById("remark").innerText = "You are Normal weight";
   else if (output >= 25 && output <= 30)
-    document.getElementById("comment").innerText = "Obese";
+    document.getElementById("remark").innerText = "You are considered Obese, we recommend higher calorie restrictions and more exercise";
   else if (output > 30)
-    document.getElementById("comment").innerText = "Overweight";
+    document.getElementById("remark").innerText = "You are considered Overweight, we recommend calorie restrictions and more exercise";
   // document.getElementById("answer").value = output;
 }
