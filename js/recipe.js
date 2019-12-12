29fd3061413fb191aaecddf12205ee1e1cbfdb13
Anna <ankaburkat@gmail.com>
2019-12-12 21:05:50 +0000

    js fix

--- js/recipe.js
+++ js/recipe.js
@@ -1,28 +1,26 @@
-$(document).ready(function() {
-  $(window).scroll(function() {
-    var y = $(window).scrollTop();
-    var steps = $('#steps').offset();
-    steps = steps.top;
+$(window).scroll(function() {
+  var y = $(window).scrollTop();
+  var steps = $('#steps').offset();
+  steps = steps.top;
 
-    if ($(document).width() > 960) {
+  if ($(document).width() > 960) {
 
-      if (y >= steps - 100) {
-        // steps bar button fade in:
-        $('#stepbar').fadeIn();
-        $('#stepbar').css({
-          'display': 'block'
-        });
+    if (y >= steps - 100) {
+      // steps bar button fade in:
+      $('#stepbar').fadeIn();
+      $('#stepbar').css({
+        'display': 'block'
+      });
 
-        // ingre desktop button fade in:
-        $('#ingreBtnBr').fadeIn();
-        $('#ingreBtnBr').css({
-          'display': 'block'
-        });
+      // ingre desktop button fade in:
+      $('#ingreBtnBr').fadeIn();
+      $('#ingreBtnBr').css({
+        'display': 'block'
+      });
 
-      } else {
-        $('#stepbar').fadeOut();
-        $('#ingreBtnBr').fadeOut();
-      }
+    } else {
+      $('#stepbar').fadeOut();
+      $('#ingreBtnBr').fadeOut();
     }
-  });
+  }
 });
