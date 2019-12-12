15df4307f5c072c4c0f27d746b8a75db48605030
Anna <ankaburkat@gmail.com>
2019-12-12 21:00:27 +0000

    new js file

---
+++ js/recipe.js
@@ -0,0 +1,28 @@
+$(document).ready(function() {
+  $(window).scroll(function() {
+    var y = $(window).scrollTop();
+    var steps = $('#steps').offset();
+    steps = steps.top;
+
+    if ($(document).width() > 960) {
+
+      if (y >= steps - 100) {
+        // steps bar button fade in:
+        $('#stepbar').fadeIn();
+        $('#stepbar').css({
+          'display': 'block'
+        });
+
+        // ingre desktop button fade in:
+        $('#ingreBtnBr').fadeIn();
+        $('#ingreBtnBr').css({
+          'display': 'block'
+        });
+
+      } else {
+        $('#stepbar').fadeOut();
+        $('#ingreBtnBr').fadeOut();
+      }
+    }
+  });
+});
