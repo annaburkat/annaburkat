20c540ea4cad307678966f8c5ac85bf0c7990770
Anna <ankaburkat@gmail.com>
2019-12-12 21:25:58 +0000

    slider js file

---
+++ js/slider.js
@@ -0,0 +1,28 @@
+// Carla slider
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("js-mythbusted-slide");
+  console.log(slides);
+  var dots = document.getElementsByClassName("js-mythbusted__dot");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" mythbusted-slider__dot--active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " mythbusted-slider__dot--active";
+}
